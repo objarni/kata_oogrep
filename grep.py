4b8fd0b09b78dcae96d7381c7426903d1b632496
Olof Bjarnason <olof.bjarnason@gmail.com>
2013-03-23 13:50:47 +0100

    HelpDisplayer class added

--- grep.py
+++ grep.py
@@ -1,6 +1,22 @@
-class Grep:
-	def __init__(self, helpdisplay):
-		self.helpdisplay = helpdisplay
+class HelpDisplayer:
+	def display_help(self):
+		print("Usage: python proc_grep.py <substring>")
+		print(".. will list all rows containing <substring>")
+		print("looking in all files in the current directory.")
 
-	def run(self, *args):
-		self.helpdisplay.display_help()
+class Grep: pass
+
+class GrepCommandLineInterpreter:
+	def __init__(self, helpdisplayer=HelpDisplayer(), grep=Grep()):
+		self.helpdisplayer = helpdisplayer
+		self.grep = grep
+
+	def run(self, args):
+		if len(args)==1:
+			self.grep.run(args[0])
+		else:
+			self.helpdisplayer.display_help()
+
+if __name__ == '__main__':
+	import sys
+	GrepCommandLineInterpreter().run(sys.argv[1:])
--- test_grep.py
+++ test_grep.py
@@ -7,18 +7,24 @@ from grep import *
 # app to quit
 
 
+# GrepCommandLineInterpreter tests
 def test_on_zero_arguments_displays_help():
-	verify_help_displayed_with_args()
+	verify_help_displayed_with_arglist([])
 
 def test_on_two_arguments_displays_help():
-	verify_help_displayed_with_args(1, 2)
+	verify_help_displayed_with_arglist([1, 2])
 
-def verify_help_displayed_with_args(*args):
-	helpdisplay = mock()
-	grep = Grep(helpdisplay)
-	grep.run(args)
-	verify(helpdisplay).display_help();
+def verify_help_displayed_with_arglist(arglist):
+	helpdisplayer = mock()
+	interpreter = GrepCommandLineInterpreter(helpdisplayer=helpdisplayer)
+	interpreter.run(arglist)
+	verify(helpdisplayer).display_help();
 
+def test_calls_grep_if_one_argument():
+	grep = mock()
+	interpreter = GrepCommandLineInterpreter(grep=grep)
+	interpreter.run(['abc'])
+	verify(grep).run('abc')
 
 
 if __name__ == '__main__':
