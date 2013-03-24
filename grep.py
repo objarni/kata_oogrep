780b283f643415e5cbdf882fb0e8d56cc8e20091
Olof Bjarnason <olof.bjarnason@gmail.com>
2013-03-24 07:38:59 +0100

    started designing Grep class, first test added

--- grep.py
+++ grep.py
@@ -4,7 +4,17 @@ class HelpDisplayer:
 		print(".. will list all rows containing <substring>")
 		print("looking in all files in the current directory.")
 
-class Grep: pass
+class GrepFileProcessor: pass
+class CurrentDirFileSource: pass
+
+class Grep:
+	def __init__(self, file_processor=GrepFileProcessor(), file_source=CurrentDirFileSource()):
+		self.file_processor = file_processor
+		self.file_source = file_source
+
+	def run(self, substring):
+		for file in self.file_source.find_files():
+			self.file_processor.process_file(file)
 
 class GrepCommandLineInterpreter:
 	def __init__(self, helpdisplayer=HelpDisplayer(), grep=Grep()):
@@ -16,6 +26,14 @@ class GrepCommandLineInterpreter:
 			self.grep.run(args[0])
 		else:
 			self.helpdisplayer.display_help()
+
+
+	void Run(List<string> args) {
+		if(args.Count == 1)
+			grep.Run(args[0]);
+		else
+			HelpDisplayer.display_help();
+	}
 
 if __name__ == '__main__':
 	import sys
--- test_grep.py
+++ test_grep.py
@@ -26,6 +26,23 @@ def test_calls_grep_if_one_argument():
 	interpreter.run(['abc'])
 	verify(grep).run('abc')
 
+# Grep tests
+# On technical regard, grep.run(substring) searches all files
+# in current directory for occurances of 'substring'. All hits
+# are reported nicely to stdout.
+# 
+# Implementationwise, Grep collaborates with a Searcher,
+# responsible for finding matches, and a Printer,
+# responsible for printing matches nicely to the console.
+
+# - make sure grep uses the Printer in the Searcher
+def test_grep_searches_using_substring():
+	searcher = mock()
+	grep = Grep(searcher=searcher)
+	grep.run('substring')
+	verify(searcher).search_using_substring('substring')
+
+def test_grep_tells
 
 if __name__ == '__main__':
 	import pytest
